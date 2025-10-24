b5754f67f11deb84fc9d3df1d71521807b8dc6cd
neworimelody <146850779+neworimelody@users.noreply.github.com>
2025-10-24 13:00:12 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// Import Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
+// TODO: import libraries for Cloud Firestore Database
+// https://firebase.google.com/docs/firestore
+import { getFirestore, collection, addDoc, getDocs, doc, updateDoc, query, where, deleteDoc } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCHr1JjEk-uhcZizVy4AuKCIwcO5hpdxFY",
+  authDomain: "artdeptinvoicing.firebaseapp.com",
+  projectId: "artdeptinvoicing",
+  storageBucket: "artdeptinvoicing.firebasestorage.app",
+  messagingSenderId: "529335133443",
+  appId: "1:529335133443:web:5975182827ef71f8d73536",
+  measurementId: "G-NHPQG19YWR"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
