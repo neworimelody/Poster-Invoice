3dd774db8bc845a27c4a6f1a0c8412a053f256f4
neworimelody <146850779+neworimelody@users.noreply.github.com>
2025-11-12 10:32:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
 // TODO: import libraries for Cloud Firestore Database
 // https://firebase.google.com/docs/firestore
-import { getFirestore, collection, addDoc, getDocs, doc, updateDoc, query, where, deleteDoc } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
+import { getFirestore, collection, addDoc, getDocs, doc, updateDoc, query, where, deleteDoc, getDoc } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCHr1JjEk-uhcZizVy4AuKCIwcO5hpdxFY",
@@ -16,7 +16,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-
 
 export const addPrice = async function(){
     var epsonWidth = document.getElementById("epson-width").value;
@@ -80,3 +79,127 @@ document.getElementById("ink-result").innerText =
   "Calculated price: $" + inkPricePerSqIn.toFixed(6) + " per sq in";
 
 }
+
+export const createInvoice = async function(){
+    var title = document.getElementById("title").value;
+    var date = document.getElementById("date").value;
+    var width = document.getElementById("width").value;
+    var height = document.getElementById("height").value;
+    var isFoam = document.getElementById("foamBoard").checked;
+    var isMat = document.getElementById("matBoard").checked;
+    var mounting = "None";
+    var quantity = document.getElementById("quantity").value; 
+    var bill = document.getElementById("bill").value;
+    var requestFrom = document.getElementById("requestFrom").value;
+    var description = document.getElementById("description").value;
+
+    if (isFoam){
+        mounting = "Foam Board"
+    }
+    else if(isMat){
+        mounting = "Mat Board"
+    }
+
+    const docRef = await addDoc(collection(db, "invoices"),{
+        title: title,
+        date: date, 
+        width: Number(width), 
+        height: Number(height), 
+        mounting: mounting,
+        quantity: Number(quantity), 
+        bill: bill, 
+        requestFrom: requestFrom, 
+        description: description,
+    });
+
+    document.getElementById("title").value = "";
+    document.getElementById("date").value = "";
+    document.getElementById("width").value = "";
+    document.getElementById("height").value = "";
+    document.getElementById("foamBoard").checked = false;
+    document.getElementById("matBoard").checked = false;
+    document.getElementById("none").checked = false;
+    document.getElementById("quantity").value = "";
+    document.getElementById("bill").value = "";
+    document.getElementById("requestFrom").value = "";
+    document.getElementById("description").value = "";
+    // console.log(docRef.title);
+    const docSnap = await getDoc(docRef);
+    console.log(docSnap.data().title);
+    sessionStorage.setItem("orderID", docSnap.id);
+    
+    location.href = 'output.html';
+
+}
+
+export const showOrders = async function(){
+  const orders = document.getElementById("orders");
+
+  orders.innerHTML = ""; 
+  
+  const ordersQuery = query(collection(db, "invoices"));
+  const ordersSnapshot = await getDocs(ordersQuery);
+  ordersSnapshot.forEach((item) => {
+    // const addToDashboard = document.getElementById("submit-order")
+    // addToDashboard.onclick = async function(){
+    //   await updateDoc(doc(db, "rectangle", item.id), {
+    //     isCompleted: true
+    //   });
+    //   showItems();
+    // }
+    const orderTile = document.createElement("div");
+    orderTile.className = "orderTile";
+
+    const orderNameLabel = document.createElement("p");
+    orderNameLabel.innerHTML = "Order Name:";
+    const orderName = document.createElement("p");
+    orderName.innerHTML = item.data().title;
+
+    // const width = document.createElement("p");
+    // width.innerHTML = item.data().width;
+    
+    // const widthLabel = document.createElement("p")
+    // widthLabel.innerHTML = "Width:"
+
+    orderTile.appendChild(orderNameLabel);
+    orderTile.appendChild(orderName);
+    // orderTile.appendChild(widthLabel);
+    // orderTile.appendChild(width);
+
+    orders.appendChild(orderTile);
+    }); //closes loop for incomplete items
+}
+
+export const createPDF = async function(){
+    const orderID = sessionStorage.getItem("orderID");
+    console.log(orderID);
+    const issuedTo = document.getElementById("issued-to");
+
+    // issuedTo.innerHTML = "";
+    const docRef = doc(db,  "invoices", orderID);
+    const docSnap = await getDoc(docRef);
+   
+    // const addToDashboard = document.getElementById("submit-order")
+    // addToDashboard.onclick = async function(){
+    //   await updateDoc(doc(db, "rectangle", item.id), {
+    //     isCompleted: true
+    //   });
+    //   showItems();
+    // }
+
+    const issuedNameOutput = document.createElement("div");
+    issuedNameOutput.className = "issuedNameOutput";
+
+    const issuedName = document.createElement("p");
+    issuedName.innerHTML = docSnap.data().bill;
+
+    issuedNameOutput.appendChild(issuedName);
+
+    issuedTo.appendChild(issuedNameOutput);
+
+ 
+    
+
+}
+
+
